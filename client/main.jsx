7bfbe4809f22d4749de020fd27720f16d2ea3174
Jared Mindock <jjmin94@gmail.com>
2025-03-31 20:47:54 -0400

    updating fixes

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom/client'
-import './style.css'
+import './styles/main.css'
 
-const App = () => {
+  const App = () => {
   const [query, setQuery] = useState('')
   const [cards, setCards] = useState([])
   const [myCards, setMyCards] = useState([])
