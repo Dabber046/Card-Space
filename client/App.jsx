e445fd583cb7860e6924be74b3444c2e96eac1a1
Jared Mindock <jjmin94@gmail.com>
2025-04-07 20:08:13 -0400

    new edit

--- client/App.jsx
+++ client/App.jsx
@@ -1,26 +1,33 @@
 // App.jsx
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './src/styles/main.css';
 
 import Layout from './src/components/layout';
 import HomePage from './src/pages/HomePage';
-import AboutPage from './src/pages/about';
+import About from './src/pages/about'; // This is the correct name
+import CardDetails from './src/pages/CardDetails';
+import Login from './src/pages/Login';
+import Register from './src/pages/Register';
+import MyCards from './src/pages/MyCards';
+import Profile from './src/pages/Profile';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="favorites" element={<FavoritesPage />} />
-          <Route path="settings" element={<SettingsPage />} />
-        </Route>
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/card/:id" element={<CardDetails />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/my-cards" element={<MyCards />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Layout>
     </Router>
   );
-};
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+export default App;
