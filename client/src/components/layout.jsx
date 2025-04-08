7b18ab6980b982cd189b874bc91a3bd9852bad5e
Jared Mindock <jjmin94@gmail.com>
2025-04-07 21:12:16 -0400

    layout change

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -21,7 +21,7 @@ const Layout = () => {
 
       <footer className="text-center py-6 border-t border-purple-700 text-gray-400 mt-10">
         <p>
-          © 2024 Jared Mindock • Contact:{' '}
+          © 2025 Jared Mindock • Contact:{' '}
           <a href="mailto:jjmin94@gmail.com" className="text-purple-300 hover:text-purple-400">
             jjmin94@gmail.com
           </a>
